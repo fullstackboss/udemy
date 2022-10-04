ca37f0bec9646894f73fcced08d2ffbc87bab843
fullstackboss <romeltejada@gmail.com>
2022-10-04 05:14:41 -0500

    Segunda actualizacion actualizacion

---
+++ dia-4/1-operadores-comparacion.py
@@ -0,0 +1,11 @@
+mibool=(1+5==6+0)
+mibool2='blanco'=='blanco'
+mibool3=100.0==100
+mibool4='blanco'=='Blanco'.lower()
+mibool5='blanco'!='Blanco'.lower()
+
+print(mibool)
+print(mibool2)
+print(mibool3)
+print(mibool4)
+print(mibool5)
